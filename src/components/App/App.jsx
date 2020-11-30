a5aa997b1bf4e5ecb031cf8d348a651b629ddd3f
Galyna Khyzhynska <gal4enock86@gmail.com>
2020-11-30 12:15:19 +0200

    добавляет стили

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+import style from './App.module.css';
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './components/App/App.jsx';
-import style from './index.module.css';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
