01b6b14c3a517b347e030643c70614a512f21e8a
Galyna Khyzhynska <gal4enock86@gmail.com>
2020-11-30 12:10:49 +0200

    добавляет папки компонентов

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# goit-react-hw-03-image-finder
+key (required) str Your API key: 18952122-26c4c8572f246f891e5c3799b
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App/App.jsx';
+import style from './index.module.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
